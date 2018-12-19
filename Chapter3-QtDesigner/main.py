56394565e634897d36a0abb8f8e02f48e1b947ac
Leo Chen <maverickcc@gmail.com>
2018-12-19 13:58:40 +0800

    add main

---
+++ Chapter3-QtDesigner/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# main.py in PyQt5-dev-book
+# Created by maverick on 2018-12-19, 12:00
+# 
+
+"""
+@version: ??
+@author: Leo Chen
+@contact: maverickcc@gmail.com
+@file: main.py
+@time: 2018-12-19 12:00
+"""
+
+# Useage:
+#   - pyuic5 -x -o widget.py widget.ui
